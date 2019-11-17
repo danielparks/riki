9ebaefbfe96a74e4ef17dfa8f47703dbb90884be
Daniel Parks <dp+git@oxidized.org>
2019-11-17 15:23:48 -0800

    clap 3.0.0-alpha.1

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,60 @@
-extern crate mustache;
+#[macro_use]
 extern crate clap;
 
-use clap::{Arg, App, AppSettings, SubCommand};
+extern crate mustache;
+
 use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io;
 use std::path::Path;
 
+#[derive(Clap)]
+struct Params {
+    /// Base of directory tree containing templates and pages
+    #[clap(short = "b", long = "base", default_value = ".")]
+    base: String,
+    #[clap(subcommand)]
+    sub_command: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    /// Render a page file
+    #[clap(name = "render")]
+    Render(Render),
+}
+
+#[derive(Clap)]
+struct Render {
+    /// Path to template to use
+    #[clap(short = "t", long = "template", default_value = "templates/default.tmpl")]
+    template: String,
+    /// Path to page file to render
+    page: String,
+}
+
+
 fn main() {
     // read page yaml
     // serve
+    let params: Params = Params::parse();
 
-    let app = App::new("rustwiki")
-        .version("0.1.0")
-        .about("Markdown wiki in Rust")
-        .setting(AppSettings::SubcommandRequired)
-        .arg(Arg::with_name("base")
-            .short("b")
-            .long("base")
-            .value_name("DIR")
-            .help("Base of directory tree containing templates and pages"))
-        .subcommand(SubCommand::with_name("render")
-            .arg(Arg::with_name("template")
-                .short("t")
-                .long("template")
-                .value_name("FILE")
-                .default_value("templates/default.tmpl")
-                .help("Template to use"))
-            .arg(Arg::with_name("page")
-                .value_name("PAGE")
-                .required(true)
-                .help("Page to render")));
-    let matches = app.get_matches();
-
-    let base = matches.value_of("base");
-    if ! base.is_none() {
-        let base_path = Path::new(base.unwrap());
+    if params.base != "." {
+        let base_path = Path::new(&params.base);
         assert!(env::set_current_dir(&base_path).is_ok());
     }
 
-    if let Some(render_matches) = matches.subcommand_matches("render") {
-        let template_path = render_matches.value_of("template").unwrap();
-        let template = mustache::compile_path(template_path).unwrap();
+    match params.sub_command {
+        SubCommand::Render(sub) => {
+            let template = mustache::compile_path(&sub.template).unwrap();
+            let page_raw = fs::read_to_string(&sub.page).unwrap();
 
-        let page_path = render_matches.value_of("page").unwrap();
-        let page_raw = fs::read_to_string(page_path).unwrap();
+            let mut data = HashMap::new();
+            data.insert("title", "hello <b>world</b>");
+            data.insert("body", &page_raw);
 
-        let mut data = HashMap::new();
-        data.insert("title", "hello <b>world</b>");
-        data.insert("body", &page_raw);
-
-        template.render(&mut io::stdout(), &data).unwrap();
+            template.render(&mut io::stdout(), &data).unwrap();
+        }
     }
 }
