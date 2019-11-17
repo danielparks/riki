07a978aaa264f5023810016c89bcd5e51959f115
Daniel Parks <dp+git@oxidized.org>
2019-11-17 03:25:20 -0800

    Use mustache for templates.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate mustache;
+
+use std::collections::HashMap;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  // command line arguments
+  // render
+  // Load template file
+  // load page file
+  // read page yaml
+  // serve
+
+  let mut data = HashMap::new();
+  data.insert("name", "world");
+
+  let template = mustache::compile_str("hello {{name}}\n").unwrap();
+  template.render(&mut io::stdout(), &data).unwrap();
 }
