0bd4bc4e88ae5097f0692af0575f5e2c88210636
Daniel Parks <dp+git@oxidized.org>
2019-11-17 03:39:50 -0800

    Pass template path optionally on command line (use clap for parsing).

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate mustache;
+extern crate clap;
 
+use clap::{Arg, App, SubCommand};
 use std::collections::HashMap;
 use std::io;
 
@@ -11,10 +13,24 @@ fn main() {
     // read page yaml
     // serve
 
+    let app = App::new("rustwiki")
+        .version("0.1.0")
+        .about("Markdown wiki in Rust")
+        .arg(Arg::with_name("template")
+            .short("t")
+            .long("template")
+            .value_name("FILE")
+            .help("Template to use")
+            .takes_value(true));
+    let matches = app.get_matches();
+
+    let template_path = matches.value_of("template")
+        .unwrap_or("templates/default.tmpl");
+    let template = mustache::compile_path(template_path).unwrap();
+
     let mut data = HashMap::new();
     data.insert("title", "hello <b>world</b>");
     data.insert("body", "<p>hello <b>world</b></p>");
 
-    let template = mustache::compile_str("<title>{{title}}</title>\n<body>{{& body}}</body>\n").unwrap();
     template.render(&mut io::stdout(), &data).unwrap();
 }
