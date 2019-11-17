305c816c5ac72972fb7f3d1f6a0ac1601f99f0c0
Daniel Parks <dp+git@oxidized.org>
2019-11-17 03:51:52 -0800

    --base

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ extern crate clap;
 
 use clap::{Arg, App, SubCommand};
 use std::collections::HashMap;
+use std::env;
 use std::io;
+use std::path::Path;
 
 fn main() {
     // command line arguments
@@ -16,6 +18,12 @@ fn main() {
     let app = App::new("rustwiki")
         .version("0.1.0")
         .about("Markdown wiki in Rust")
+        .arg(Arg::with_name("base")
+            .short("b")
+            .long("base")
+            .value_name("DIR")
+            .help("Base of directory tree containing templates and pages")
+            .takes_value(true))
         .arg(Arg::with_name("template")
             .short("t")
             .long("template")
@@ -23,6 +31,12 @@ fn main() {
             .help("Template to use")
             .takes_value(true));
     let matches = app.get_matches();
+
+    let base = matches.value_of("base");
+    if ! base.is_none() {
+        let base_path = Path::new(base.unwrap());
+        assert!(env::set_current_dir(&base_path).is_ok());
+    }
 
     let template_path = matches.value_of("template")
         .unwrap_or("templates/default.tmpl");
