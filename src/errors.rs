ad9ceade859a1acf80ec83c82cda122bae5b14c9
Daniel Parks <dp+git@oxidized.org>
2025-08-25 13:29:15 -0700

    Sort variants of `riki::Error`.

--- src/errors.rs
+++ src/errors.rs
@@ -8,15 +8,25 @@ use thiserror::Error; // doesn’t conflict with the enum.
 /// Error type for the crate.
 #[derive(Debug, Error)]
 pub enum Error {
+    /// Failed to bind to socket
+    #[error("Failed to bind to socket on {address:?}")]
+    BindError {
+        /// The original error
+        source: io::Error,
+
+        /// The address the socket could not bind on
+        address: String,
+    },
+
     /// IO error
     #[error("IO error")]
     Io(#[from] io::Error),
 
-    /// Failed to read page file
-    #[error("Failed to read page file")]
-    ReadPageFile {
-        /// The original error.
-        source: io::Error,
+    /// Failed to render page metadata
+    #[error("Failed to render page metadata")]
+    MetadataRender {
+        /// The original error
+        source: serde_yaml::Error,
     },
 
     /// Failed to render page body with [`handlebars`]
@@ -32,30 +42,20 @@ pub enum Error {
         template: String,
     },
 
-    /// Failed to compile template with [`handlebars`]
-    #[error("Failed to compile template at: {0:?}")]
-    TemplateCompile(#[from] handlebars::TemplateError),
-
-    /// Failed to render page metadata
-    #[error("Failed to render page metadata")]
-    MetadataRender {
-        /// The original error
-        source: serde_yaml::Error,
-    },
-
     /// Failed to parse page metadata
     #[error("Failed to parse page metadata")]
     ParsePageMetadata(#[from] serde_yaml::Error),
 
-    /// Failed to bind to socket
-    #[error("Failed to bind to socket on {address:?}")]
-    BindError {
-        /// The original error
+    /// Failed to read page file
+    #[error("Failed to read page file")]
+    ReadPageFile {
+        /// The original error.
         source: io::Error,
-
-        /// The address the socket could not bind on
-        address: String,
     },
+
+    /// Failed to compile template with [`handlebars`]
+    #[error("Failed to compile template at: {0:?}")]
+    TemplateCompile(#[from] handlebars::TemplateError),
 }
 
 /// `Result` type for this crate.
